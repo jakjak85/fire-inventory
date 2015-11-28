b4e91a273a44ded6a6dc0bacc199b1c7f3b35c0e
jakjak85 <jakjak@gmail.com>
2015-11-28 18:16:02 -0500

    adding watcher for currently seen data

--- app/actors/PersonnelActor.java
+++ app/actors/PersonnelActor.java
@@ -3,11 +3,10 @@ package actors;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import actors.PersonnelActor.DetectChange;
 import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
-import akka.routing.Broadcast;
 import dataAccess.DataAccessor;
 import dataAccess.MongoDBDataAccessor;
 import play.Logger;
@@ -20,6 +19,7 @@ public class PersonnelActor extends UntypedActor {
 	private final ActorRef out;
 
 	private DataAccessor accessor = MongoDBDataAccessor.getAccessor();
+	private DetectChange changeWatcher;
 
 	public PersonnelActor(ActorRef out) {
 		this.out = out;
@@ -67,62 +67,108 @@ public class PersonnelActor extends UntypedActor {
 
 	private void queryAnyAction() {
 		String result = accessor.getAnyRecord();
-		if(result != null)
-		{
+		Object obj = JSONValue.parse(result);
+		JSONObject jObj = (JSONObject) obj;
+		String id = getProperty(jObj, "_id");
+		if (result != null) {
 			out.tell(result, self());
-			
-		}
-		else
-		{
+
+		} else {
 			out.tell("{}", self());
 		}
+		startWatch(id, result);
 	}
-	
+
 	private void queryAction(JSONObject msg) {
 		String fname = getProperty(msg, "firstName");
 		String lname = getProperty(msg, "lastName");
 		String result = accessor.getRecord(fname, lname);
-		if(result != null)
-		{
+		Object obj = JSONValue.parse(result);
+		JSONObject jObj = (JSONObject) obj;
+		String id = getProperty(jObj, "_id");
+		if (result != null) {
 			out.tell(result, self());
-			
-		}
-		else
-		{
+		} else {
 			out.tell("{}", self());
 		}
+		startWatch(id, result);
 	}
-	
+
 	private void queryIdAction(JSONObject msg) {
 		String id = getProperty(msg, "_id");
 		String result = accessor.getRecordById(id);
-		if(result != null)
-		{
+		if (result != null) {
 			out.tell(result, self());
-		}
-		else
-		{
+		} else {
 			out.tell("{}", self());
 		}
+		startWatch(id, result);
 	}
-	
+
 	private void addAction(JSONObject msg) {
 		accessor.addRecord(msg);
+		out.tell("{}", self());
+		stopWatch();
 	}
+
 	private void removeAction(JSONObject msg) {
 		String id = getProperty(msg, "_id");
 		accessor.removeUser(id);
 		out.tell("{}", self());
+		stopWatch();
 	}
-	
+
 	private void updateAction(JSONObject msg) {
-		accessor.updateRecord(msg);		
+		accessor.updateRecord(msg);
 	}
-	
-	private String getProperty(JSONObject msg, String propId)
-	{
+
+	private String getProperty(JSONObject msg, String propId) {
 		Object obj = msg.get(propId);
 		String str = (String) obj;
 		return str;
+	}
+
+	private void startWatch(String id, String object) {
+		if (changeWatcher != null) {
+			stopWatch();
+		}
+		new Thread(new DetectChange(id, object)).start();
+	}
+
+	private void stopWatch() {
+		if (changeWatcher != null) {
+			changeWatcher.stop();
+			changeWatcher = null;
+		}
+	}
+
+	class DetectChange implements Runnable {
+		String objectWatch = "";
+		String objectId = "";
+		volatile boolean keepGoing = true;
+
+		DetectChange(String id, String object) {
+			objectId = id;
+			objectWatch = object;
+		}
+
+		public void run() {
+			while (keepGoing) {
+				try {
+					Thread.sleep(1000);
+					String newObject = accessor.getRecordById(objectId);
+					if (!objectWatch.equals(newObject)) {
+						out.tell(newObject, self());
+						objectWatch = newObject;
+					}
+				} catch (Exception e) {
+					Logger.error("Error sleeping", e);
+				}
+			}
+		}
+
+		public void stop() {
+			keepGoing = false;
+		}
 	}
 }
